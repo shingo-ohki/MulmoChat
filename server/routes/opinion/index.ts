b287f01d69d41fea4c84974370cfb7127efd488c
Shingo OHKI <truego@gmail.com>
2025-10-29 13:56:57 +0900

    Whisper API を使ったものは削除

--- server/routes/opinion/index.ts
+++ server/routes/opinion/index.ts
@@ -1,15 +1,7 @@
 import { Router } from "express";
-import chatRouter from "./chat";
-import voiceRouter from "./voice";
 import logRouter from "./log";
 
 const router = Router();
-
-// Mount chat routes: /api/opinion/chat
-router.use("/chat", chatRouter);
-
-// Mount voice routes: /api/opinion/voice
-router.use("/voice", voiceRouter);
 
 // Mount log routes: /api/opinion/log
 router.use("/log", logRouter);
